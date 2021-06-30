23f24bdf192cde404a26e82f22683745680d5d8e
leonop <zicheng@mail.usf.com>
2021-06-30 01:28:43 -0400

    master

---
+++ backtest/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+
+     name='ZX_backtesting',  
+
+     version='0.1',
+
+     author="Zicheng Xiao",
+
+     description="<back testing system>",
+
+     long_description='back testing system for algo trading',
+
+     long_description_content_type="text/markdown",
+
+     packages=setuptools.find_packages(),
+
+ )
